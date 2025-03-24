2046343a0060efafa2d8c2c213f30aa6dac98c1a
wen-05 <aiot63324@gmail.com>
2025-03-24 20:55:04 +0800

    feat: add upload image to cloudinary

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const coachRouter = require('./coach')
 const userRouter = require('./users')
 const adminRouter = require('./admin')
 const coursesRouter = require('./courses')
+const uploadRouter = require('./upload')
 
 router.use('/credit-package', creditPackageRouter)
 router.use('/coaches/skill', skillRouter)
@@ -14,5 +15,6 @@ router.use('/coaches', coachRouter)
 router.use('/users', userRouter)
 router.use('/admin/coaches', adminRouter)
 router.use('/courses', coursesRouter)
+router.use('/upload', uploadRouter)
 
 module.exports = router
